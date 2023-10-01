4d5a790b4271d0642219feda727c2b30bc31d187
Margaret Lu <margarel@andrew.cmu.edu>
2023-09-30 21:03:17 -0400

    added to cart and console

--- solution-hw4/js/index.js
+++ solution-hw4/js/index.js
@@ -1,11 +1,16 @@
-/* https://www.tutorialrepublic.com/faq/how-to-include-a-javascript-file-in-another-javascript-file.php */
-import { rolls } from "./rollsData";
+let cardId = "";
+let a = 2;
+let cart = [];
 
-/* https://www.w3schools.com/js/js_window_location.asp */
-function navtoProductDetail(rollName) {
-    window.location.href = `product-detail.html?roll=${(rollName)}`
+for (i = 0; i < rolls.length; i++) {
+    cardId = document.getElementById((Object.keys(rolls)[i]).toLowerCase());
+    cardId.addEventListener("click", navtoProductDetail(cardId));
 }
 
-for (i = 0; i < rollsData.length; i++) {
-    
+console.log(a);
+console.log(document.getElementById((Object.keys(rolls)[0]).toLowerCase()));
+
+/* https://www.w3schools.com/js/js_window_location.asp */
+function navtoProductDetail(cardId) {
+    window.location.href = `product-detail.html?roll=${(cardId)}`; // idk if this works lowkey
 }
--- solution-hw4/js/product-detail.js
+++ solution-hw4/js/product-detail.js
@@ -1,5 +1,24 @@
+const queryString = window.location.search;
+/* console.log(queryString); // ?=roll(blah blah blah) */
+
+const params = new URLSearchParams(queryString);
+/* console.log(params); // object */
+
+const rollType = params.get("roll");
+/* console.log(rollType); // Apple or like Double-Chocolate */
+
+/* Changes the title of the page to match the product name */
+const headerElement = document.querySelector("#type");
+headerElement.innerText = rollType + " Cinnamon Roll";
+
+/* Changes the image to match the correct product */
+const rollImage = document.querySelector("#roll-image");
+rollImage.src = "../../assets/products/" + rollType.toLowerCase() + "-cinnamon-roll.jpg";
+/* console.log(rollType);
+console.log(rollImage.src); */
+
 /* All the glazing options */
-let glazingOptions = [
+const glazingOptions = [
     {name: "Keep original", price: 0.00},
     {name: "Sugar milk", price: 0.00},
     {name: "Vanilla milk", price: 0.50},
@@ -7,7 +26,7 @@ let glazingOptions = [
 ];
 
 /* All the pack size options with their prices */
-let packSizeOptions = [
+const packSizeOptions = [
     {name: "1", price: 1.00},
     {name: "3", price: 3.00},
     {name: "6", price: 5.00},
@@ -23,10 +42,36 @@ function populateDropdown(id, options) {
         optionElement.value = option.price;
         dropdown.appendChild(optionElement);
     });
+
+    dropdown.addEventListener("change", () => {
+        const selectedOption = dropdown.options[dropdown.selectedIndex].textContent;
+        /* console.log(selectedOption); */
+    })
 }
 
 populateDropdown("glazing", glazingOptions);
 populateDropdown("pack-size", packSizeOptions);
+
+
+
+
+
+/* Checking if the cinnamon roll flavor is  */
+const rollsArray = Object.entries(rolls);
+let type;
+let basePrice;
+
+for (let i = 0; i < rollsArray.length; i++) {
+    type = rollsArray[i][0];
+    if (type == rollType) {
+        basePrice = rollsArray[i][1].basePrice;
+        break;
+    }
+}
+
+document.getElementById("total-price").textContent = "$ " + basePrice;
+
+
 
 /* Updates the price on change of dropdown selection */
 function updatePrice() {
@@ -34,7 +79,6 @@ function updatePrice() {
     /* https://www.w3schools.com/jsref/jsref_number_parsefloat.asp */
     let glazingPrice = parseFloat(document.getElementById("glazing").value);
     let packPrice = parseFloat(document.getElementById("pack-size").value);
-    let basePrice = 2.49;
 
     let total = (basePrice + glazingPrice) * packPrice;
 
@@ -42,8 +86,35 @@ function updatePrice() {
     document.getElementById("total-price").textContent = "$ " + total.toFixed(2); // toFixed --> to the second decimal place
 }
 
+
 const glazeSelect = document.getElementById("glazing");
 glazeSelect.addEventListener("change", updatePrice);
 
 const packSelect = document.getElementById("pack-size");
 packSelect.addEventListener("change", updatePrice);
+
+let cart = [];
+
+class Roll {
+    constructor(rollType, glazeSelect, packSelect, basePrice) {
+        this.type = rollType;
+        this.glazing = glazeSelect;
+        this.size = packSelect;
+        this.basePrice = basePrice;
+    }
+}
+
+const cartButton = document.getElementById("btn-cart");
+cartButton.addEventListener("click", addToCart);
+
+function addToCart() {
+    let glazingId = document.getElementById("glazing");
+    let glazingSelected = glazingId.options[glazingId.selectedIndex].textContent;
+
+    let packID = document.getElementById("pack-size");
+    let packSelected = packID.options[packID.selectedIndex].textContent;
+
+    const newRoll = new Roll(rollType, glazingSelected, packSelected, basePrice);
+    cart.push(newRoll);
+    console.log(cart);
+}
