a2694e33c12fea9fca2c7d6023b7e7fa5ae37809
Margaret Lu <margarel@andrew.cmu.edu>
2023-11-29 11:46:45 -0500

    Added NCR images

--- solution-hw7/js/main.js
+++ solution-hw7/js/main.js
@@ -1,1 +1,17 @@
-console.log(innerWidth);
+// let card = document.getElementById("ncr-project");
+// new simpleParallax(card);
+
+$(window).scroll(function() {
+    $('#ncr-project').each(function(){
+    let imagePos = $(this).offset().top;
+
+    let topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+            $(this).addClass("slideUp");
+        }
+    });
+});
+
+$('#ncr-project').click(function() {
+    $(this).addClass("slideUp");
+});
